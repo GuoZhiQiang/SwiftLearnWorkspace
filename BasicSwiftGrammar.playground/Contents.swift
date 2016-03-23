60c444bc8b4b1d2a02d9e1c481fbb9e1dfc43c71
coding/ios <1029549847@qq.com>
2016-03-23 19:25:43 +0800

    four push

--- BasicSwiftGrammar.playground/Contents.swift
+++ BasicSwiftGrammar.playground/Contents.swift
@@ -444,6 +444,25 @@ if let convertRank = Rank(rawValue: 3) {
     let threeDescription = convertRank.simpleDescription()
 }
 
+/*********/
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() ->String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.Hearts
+let heartsDes = hearts.simpleDescription()
+
 
 
 
