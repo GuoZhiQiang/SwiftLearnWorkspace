441464c766e409982d57c9c209d3fe63eb6cff5f
coding/ios <1029549847@qq.com>
2016-03-29 19:47:27 +0800

    ten push

--- BasicSwiftGrammar.playground/Contents.swift
+++ BasicSwiftGrammar.playground/Contents.swift
@@ -530,6 +530,7 @@ class SomeClass: Shape,FirstProtocol,AnotherProtocol {
 protocol SomeProtocol {
     var mustBeSettable: Int {get set}
     var doNotNeedToBeSettable: Int {get}
+    func someTypeMethod()
 }
 protocol FullyNamed {
     var fullName: String {get}
@@ -555,7 +556,34 @@ class Starship: FullyNamed {
 var starShip = Starship(name: "Enterprise", prefix: "USS")
 print(starShip.fullName)
 
+protocol RandomNumberGenerator {
+    func random() ->Double
+}
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 2973.0
+    func random() -> Double {
+        lastRandom = ((lastRandom*a + c) % m)
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Random number : \(generator.random())")
+print("Another Random number: \(generator.random())")
 
+/*****value type****/
+var arrayValue = [12,43,55,1]
+var arrayCopyValue = arrayValue
+print(unsafeAddressOf(arrayValue))
+print(unsafeAddressOf(arrayCopyValue))
+
+/*****扩展****/
+extension Starship {
+    // new Functionality to add to Starship goes here
+}
 
 
 
